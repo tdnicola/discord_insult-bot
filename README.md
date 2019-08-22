d11b3a8677dae8e5fc22256fe5f807f8904b91e1
tdnicola <tdnicola@gmail.com>
2019-08-22 16:42:30 -0400

    added !praise command. added reaction to requesters message. changed config start ups for heroku.

--- README.md
+++ README.md
@@ -4,4 +4,8 @@ Uses https://rapidapi.com/Lakerolmaker/api/insult-generator/endpoints API.
 
 !insult @person
 
+After insulted you can praise them to make them feel better with:
+!praise @person
+
+https://discordapp.com/api/oauth2/authorize?client_id=613364681750609943&permissions=0&scope=bot
 deployed on heroku
--- bot.js
+++ bot.js
@@ -26,23 +26,41 @@ client.on('message', message => {
         req.end(function (res) {
             if (res.error) throw new Error(res.error);
             var insult = res.raw_body.toLowerCase();
-            let member = message.mentions.members.first()
+            let member = message.mentions.members.first();
 
             if (member == '' || member == null)
                 message.reply('Dude you had to include two things and you screwed that up...');
             else {
-            message.channel.send('@' + member.displayName + ', ' + insult)
+            message.channel.send('@' + member.displayName + ', ' + insult + '.');
+            message.react("🔥");
+            
             }
         });
     }
-    if (message.author != client.user ){
-        return
+
+     if(message.content.startsWith(`${prefix}praise`)) {
+        var unirest = require("unirest");
+    
+        var req = unirest("GET", "https://complimentr.com/api");
+        
+        req.end(function (res) {
+            if (res.error) throw new Error(res.error);
+            var praise = String(res.body.compliment);
+            let member = message.mentions.members.first();
+
+            if (member == '' || member == null)
+                message.reply('Dude you had to include two things and you screwed that up...');
+            else {
+            message.channel.send('I\'m sorry I insulted you @' + member.displayName + ', ' + praise + '.');
+            message.react("🙏");
+            }
+        });
     }
-        message.react("🔥")
 });
+
 
 client.on('ready', () => {
     client.user.setActivity('for Noobs. !insult @name', { type: 'Watching' });
 });
 
-client.login(token)
+client.login(token);
