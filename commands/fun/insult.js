45a99c93f8bc5de69594c8a193ad0ae149381f07
Tony Nicola <46770472+tdnicola@users.noreply.github.com>
2025-04-30 19:15:19 -0700

    adding commands and cleanup

--- commands/fun/insult.js
+++ commands/fun/insult.js
@@ -1,6 +1,7 @@
 const { request } = require("undici");
 const { SlashCommandBuilder } = require("discord.js");
-const API_BASE_URL = process.env.API_BASE_URL || 'http://host.docker.internal:5000';
+const { updateInteractionStats } = require('../../db'); 
+const API_BASE_URL = process.env.API_BASE_URL || 'http://localhost:5000';
 
 
 module.exports = {
@@ -11,20 +12,31 @@ module.exports = {
             option.setName("user").setDescription("Let's insult somebody...")
         ),
     async execute(interaction) {
+        const sender = interaction.user;
         const insultURL = await request(
             `${API_BASE_URL}/insult`
         );
         const body = await insultURL.body.json(); 
         const insult = body.message; 
-
+        const target = interaction.options.getUser('user') || sender;
+        console.log(sender);
+        console.log(target);
         await interaction.deferReply();
 
         const message = await interaction.editReply(
             `${
-                interaction.options.getUser("user") ?? interaction.user
+                target
             } ${insult}`
         );
         message.react("🔥");
+
+        await updateInteractionStats(
+            sender.id,
+            sender.username,
+            target.id,
+            target.username,
+            'insult'
+          );
 
     },
 };
